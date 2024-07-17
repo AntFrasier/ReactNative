bcbfc20debfa968d94f0bc62ed3173d33e6acee5
Cyril Maranber <cyril.maranber@spliterz.com>
2024-07-17 10:11:40 +0200

    setUp nativeWind ok

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className = "flex-1 items-center justify-center bg-white"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-3xl">My App !</Text>
+      <Link href="/profile" style = {{color : "blue"}}>Profile</Link>
     </View>
   );
 }
